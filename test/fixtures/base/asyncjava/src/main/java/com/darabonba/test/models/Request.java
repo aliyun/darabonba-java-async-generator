ac6880f0c31ed9c176738be84cdae142d2238701
yndu13 <yndu13@gmail.com>
2021-09-16 16:51:15 +0800

    add default class name && add directories

---
+++ test/fixtures/base/asyncjava/src/main/java/com/darabonba/test/models/Request.java
@@ -0,0 +1,12 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.darabonba.test.models;
+
+import darabonba.core.RequestModel;
+
+public class Request extends RequestModel {
+
+    protected Request(Builder builder) {
+        super(builder);
+    }
+
+}

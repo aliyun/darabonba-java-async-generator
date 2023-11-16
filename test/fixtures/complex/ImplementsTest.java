0f73ad3eedfc188c469b0ed2df7c19e4658f4e04
yndu13 <yndu13@gmail.com>
2023-11-16 13:12:17 +0800

    support setters and getters for models

---
+++ test/fixtures/complex/ImplementsTest.java
@@ -0,0 +1,26 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun;
+
+import com.aliyun.models.*;
+import darabonba.core.async.*;
+import darabonba.core.sync.*;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface ImplementsTest {
+
+    static NameTestBuilder builder() {
+        return new NameTestBuilder();
+    }
+
+    static ImplementsTest create() {
+        return builder().build();
+    }
+
+    CompletableFuture<void> testSubModel(ComplexRequest.ComplexRequestPart part, java.util.List<java.util.List<String>> complexList);
+
+    CompletableFuture<java.util.List<String>> hello(java.util.Map<String, ?> request, java.util.List<String> strs);
+
+    CompletableFuture<String> templateString();
+
+}
